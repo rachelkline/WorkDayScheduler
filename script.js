6404067e6e8ece02b13e2e8d960e527765d1c450
Rachel Kline <rach3lkline@gmail.com>
2020-09-11 16:16:49 -0400

    Updated CSS and localStorage properly working

--- script.js
+++ script.js
@@ -12,118 +12,100 @@ var workHours = {
 };
 
 $(document).ready(function () {
+    displayDateDay();
     //retrieve items from local storage when the page loads
     if (!localStorage.getItem('workHours')) {
         updateCalendar(workHours);
     } else {
         updateCalendar(JSON.parse(localStorage.getItem('workHours')));
     }
+
 });
-//Add the date to the page using moments.js
-var time = moment().format('LT');
-console.log(time);
+    //Add the date to the page using moments.js
+    var time = moment().format('LT');
+    console.log(time);
 
-displayDateDay();
-
-//function to display current date/day
-function displayDateDay() {
-    var weekDay = moment().format("dddd");
-    var date = moment().format("MMM Do YYYY");
-    $("#currentDay").append(weekDay + ", " + date);
-    console.log(weekDay + date);
-
-}
-
-//for loop to determine what time blocks get what classes
-var hourProgress = 1;
-for (var property in workHours) {
-    var userInput = "#text-entry" + hourProgress;
-    $(userInput).text(workHours[property]);
-    var time = "#time" + hourProgress;
-    var presentHour = moment().hour();
-    var timeValue = $(time).text();
-    var timePNumbers = hourFromHourValue(timeValue);
-    if (timePNumbers < presentHour) {
-        $(userInput).addClass("past");
-    } else if (timePNumbers > presentHour) {
-        $(userInput).addClass("future");
-    } else {
-        $(userInput).addClass("present");
-    }
-    hourProgress++;
-}
-
-$("button").click(function () {
-    value = $(this).siblings("textarea").val();
-    hourValue = $(this).siblings("div").text();
-
-    saveUserInput(hourValue, value);
-})
-
-function hourFromHourValue(hourValue) {
-    switch (hourValue) {
-        case "9AM": return 9;
-        case "10AM": return 10;
-        case "11AM": return 11;
-        case "12PM": return 12;
-        case "1PM": return 13;
-        case "2PM": return 14;
-        case "3PM": return 15;
-        case "4PM": return 16;
-        case "5PM": return 17;
-    }
-}
-
-
-function loadDataset() {
-    result = localStorage.getItem("workHours")
-    return (result ? result : workHours);
-}
-
-function initializeLS() {
-    localStorage.setItem("workHours", JSON.stringify(workHours));
-}
-
-function saveToLS(dayObj) {
-    localStorage.setItem('workHours', JSON.stringify(dayObj));
-}
-
-function saveUserInput(hourValue, val) {
-    if (!localStorage.getItem("workHours")) {
-        initializeLS();
+    //display current date/day using moment.js
+    function displayDateDay() {
+        var weekDay = moment().format("dddd");
+        var date = moment().format("MMM Do YYYY");
+        $("#currentDay").append(weekDay + ", " + date);
+        console.log(weekDay + date);
     }
 
+    //for loop to determine what time blocks get what classes
+    var hourProgress = 1;
+    for (var property in workHours) {
+        var userInput = "#text-entry" + hourProgress;
+        $(userInput).text(workHours[property]);
+        var time = "#time" + hourProgress;
+        var presentHour = moment().hour();
+        var timeValue = $(time).text();
+        var timePNumbers = hourFromHourValue(timeValue);
+        if (timePNumbers < presentHour) {
+            $(userInput).addClass("past");
+        } else if (timePNumbers > presentHour) {
+            $(userInput).addClass("future");
+        } else {
+            $(userInput).addClass("present");
+        }
+        hourProgress++;
+    }
 
-    var workDay = JSON.parse(localStorage.getItem("workHours"));
-    workDay[hourValue] = val
+    //when the button is clicked, the user input is saved to that specific time block
+    $("button").click(function () {
+        value = $(this).siblings("textarea").val();
+        hourValue = $(this).siblings("div").text();
 
-    saveToLS(workDay);
-
-}
-
-function updateCalendar(dayObject) {
-    $(".calendar-row").each(function (index) {
-        var res = $(this).children("div");
-        $(this).children("textarea").text(dayObject[res.text()]);
+        saveUserInput(hourValue, value);
     })
-}
+
+    //change the text to physical numbers in military time
+    function hourFromHourValue(hourValue) {
+        switch (hourValue) {
+            case "9AM": return 9;
+            case "10AM": return 10;
+            case "11AM": return 11;
+            case "12PM": return 12;
+            case "1PM": return 13;
+            case "2PM": return 14;
+            case "3PM": return 15;
+            case "4PM": return 16;
+            case "5PM": return 17;
+        }
+    }
 
 
+    function Dataset() {
+        result = localStorage.getItem("workHours")
+        return (result ? result : workHours);
+    }
+
+    function initializeLS() {
+        localStorage.setItem("workHours", JSON.stringify(workHours));
+    }
+
+    function saveToLS(dayObj) {
+        localStorage.setItem('workHours', JSON.stringify(dayObj));
+    }
+
+    function saveUserInput(hourValue, val) {
+        if(!localStorage.getItem('workHours')) {
+            initializeLS();
+        }
 
 
-// var key = "9am";
-// var task = "This is an example note!";
+        var workDay = JSON.parse(localStorage.getItem('workHours'));
+        workDay[hourValue] = val
 
-// var timeBlocks = ["9am", "10am", "11am", "5pm"]
+        saveToLS(workDay);
 
-// for (var i=0; i < timeBlocks.length; i++) {
-//     var task = localStorage.getItem(timeBlocks[i]);
-//     // change text of relavent timeBlock to task here
-// }
+    }
 
-// //timeBlock: "9am" -> "5pm"
-// //tawsk: any String
-// function saveTask(timeBlock,task) {
-//     localStorage.setItem(timeBlock, task);
-// }
-// You're setting the timeBlock back to local storage, so if one thing is changed everything else will stay but that one thing will change
+    function updateCalendar(hourObj) {
+        $(".calendar-row").each(function(index) {
+            var res = $(this).children("div");
+            $(this).children("textarea").text(hourObj[res.text()]);
+        })
+    }
+
